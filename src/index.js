7d9edfef7afaff6c56262dad0e9b493a242e90b0
Korey Hines <KHines@paqinc.com>
2024-01-19 11:08:26 -0800

    get

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import { hydrateRoot } from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-hydrateRoot(
-  document.getElementById('root'),
-  <App />
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
